c30c355a3f0c9869f15bae82cbb379173efeeb80
Shane <Shane.D.Bogard@gmail.com>
2018-04-23 18:15:54 -0500

    Empty Employee Classes

---
+++ src/application/model/Driver.java
@@ -0,0 +1,10 @@
+package application.model;
+
+public class Driver extends Employee {
+
+    public Driver(String name, int id, int type) {
+        super(name, id, type);
+    }
+
+
+}
---
+++ src/application/model/Manager.java
@@ -0,0 +1,10 @@
+package application.model;
+
+public class Manager extends Employee {
+
+    public Manager(String name, int id, int type) {
+        super(name, id, type);
+    }
+
+
+}
