376f37f742c516ac4ee3b9ef71068bc13aa26777
Daniel <danielghazvini@gmail.com>
2018-04-24 13:01:25 -0500

    Changes to login

--- src/application/controller/CustomerLoginController.java
+++ src/application/controller/CustomerLoginController.java
@@ -1,5 +1,7 @@
 package application.controller;
 
+import java.io.IOException;
+
 import application.Main;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -29,8 +31,24 @@ public class CustomerLoginController implements EventHandler <ActionEvent> {
 		if (event.getSource() == submit) {
 			System.out.println("Test!");
 			
-			if (cusEmail.getText().equals("1") && cusPassword.getText().equals("1")) {
+			if (cusEmail.getText().equals("test") && cusPassword.getText().equals("test")) {
 				loginStatus.setText("Login Successful");
+				try {
+					Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+					
+					// Set the scene to stage and show the stage to the user
+					Main.stage.setTitle("GDS - Manager");
+					Main.stage.setResizable(true);
+					Main.stage.setHeight(400);
+					Main.stage.setMinHeight(400);
+					Main.stage.setMinWidth(600);
+					Main.stage.setScene(new Scene (root));
+					Main.stage.show();	
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
 			//	Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
 			} else {
 				loginStatus.setText("Invalid credentials");
